cbcdf86cff90a68bf0b086f7d9b9ff351d2f7801
Talal Tabia <mail@talaltabia.com>
2020-12-12 15:28:51 +0100

    add fargate application load balancing

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,3 +2,13 @@ output "vpc_default_id" {
   description = "delivers the created default vpc id"
   value = aws_vpc.vpc_main.id
 }
+
+output "sg_default_id" {
+  description = "delivers the network default security group"
+  value = aws_default_security_group.sg.id
+}
+
+output "subnet_ids" {
+  description = "delivers all network subnet ids"
+  value = [aws_subnet.sn_00_eu_central_1a_vpc_main.id, aws_subnet.sn_01_eu_central_1b_vpc_main.id, aws_subnet.sn_02_eu_central_1c_vpc_main.id]
+}
--- template.tf
+++ template.tf
@@ -13,6 +13,11 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
+data "aws_route53_zone" "zone_default" {
+  name         = "casasky.de"
+  private_zone = false
+}
+
 module "network_default" {
   source = "./modules/network/"
   vpc_cidr_block = var.vpc_cidr_block
@@ -104,4 +109,85 @@ resource "aws_security_group" "sg_template_rds" {
     self             = false
     to_port          = 5432
   }
+}
+
+resource "aws_lb" "alb_earthws_fg" {
+  name               = "earthws-fg-alb"
+  load_balancer_type = "application"
+  security_groups    = [module.network_default.sg_default_id, aws_security_group.sg_earthws_fg_alb.id]
+  subnets            = module.network_default.subnet_ids
+}
+
+resource "aws_lb_target_group" "target_group_alb_earthws_fg" {
+  name     = "earthws-fg-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = module.network_default.vpc_default_id
+}
+
+resource "aws_lb_listener" "listener_alb_earthws_fg" {
+  load_balancer_arn = aws_lb.alb_earthws_fg.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_alb_earthws_fg.arn
+  }
+}
+
+resource "aws_lb_listener" "ssl_listener_alb_earthws_fg" {
+  load_balancer_arn = aws_lb.alb_earthws_fg.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-central-1:182355820400:certificate/a2bc7f64-03a5-42d0-a021-da8210850018"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_alb_earthws_fg.arn
+  }
+}
+
+resource "aws_acm_certificate" "cert_ssl" {
+  domain_name       = "earthws.alb.casasky.de"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    "application" = "earthws"
+  }
+}
+
+# This resource is for additional certificates and does not replace the default certificate on the listener.
+resource "aws_lb_listener_certificate" "listener_cert_ssl" {
+  listener_arn    = aws_lb_listener.ssl_listener_alb_earthws_fg.arn
+  certificate_arn = aws_acm_certificate.cert_ssl.arn
+}
+
+# This resource implements a part of the validation workflow. It does not represent a real-world entity in AWS, therefore changing or deleting this resource on its own has no immediate effect.
+resource "aws_acm_certificate_validation" "validation_cert_ssl" {
+  certificate_arn         = aws_acm_certificate.cert_ssl.arn
+  validation_record_fqdns = [for record in aws_route53_record.record_alb_earthws : record.fqdn]
+}
+
+resource "aws_route53_record" "record_alb_earthws" {
+  for_each = {
+  for dvo in aws_acm_certificate.cert_ssl.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone_default.zone_id
 }
