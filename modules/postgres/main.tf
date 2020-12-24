d1e9aed82c2f734c49d05f4f14526b957781586a
Talal Tabia <mail@talaltabia.com>
2020-12-24 20:53:01 +0100

    remove storage encryption, because it's not supported in micro instance

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -35,7 +35,6 @@ resource "aws_db_instance" "postgres" {
   max_allocated_storage  = 1000
   username               = "postgres"
   password               = var.password
-  storage_encrypted      = true
   vpc_security_group_ids = [aws_security_group.postgres.id, var.default_sg_id]
   db_subnet_group_name = aws_db_subnet_group.postgres.name
   iam_database_authentication_enabled = true
